b6a8668b904f799de251fcf3f2c1fdc837ea4190
honma <honmatchy@icloud.com>
2021-10-11 16:29:49 +0900

    Initial Commit

---
+++ Opesketch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Opesketch
+//
+//  Created by 本間光 on 2021/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Opesketch/OpesketchApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpesketchApp.swift
+//  Opesketch
+//
+//  Created by 本間光 on 2021/10/11.
+//
+
+import SwiftUI
+
+@main
+struct OpesketchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
