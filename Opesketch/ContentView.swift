e5d3ebd0b5a279ccf801a9e8c1056d137f997de6
honma <honmatchy@icloud.com>
2021-10-12 23:14:48 +0900

    ボタンの配置

--- Opesketch/ContentView.swift
+++ Opesketch/ContentView.swift
@@ -9,8 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            VStack {
+                Rectangle()
+                    .foregroundColor(.white)
+                HStack {
+                    Group {
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                            Image(systemName: "play.circle")
+                                .font(.system(size: 30))
+                            Text("play")
+                        }
+                        .padding()
+                        .foregroundColor(.black)
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                            Image(systemName: "pause.circle")
+                                .font(.system(size: 30))
+                            Text("pause")
+                        }
+                        .padding()
+                        .foregroundColor(.black)
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                            Image(systemName: "stop.circle")
+                                .font(.system(size: 30))
+                            Text("stop")
+                        }
+                        .padding()
+                        .foregroundColor(.black)
+                    }
+                }
+            }
+            Divider()
+            HStack {
+                Rectangle()
+                    .foregroundColor(.white)
+                VStack {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image(systemName: "pencil")
+                            .font(.system(size: 40))
+                    }
+                    .padding()
+                    .foregroundColor(.gray)
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image(systemName: "scissors")
+                            .font(.system(size: 40))
+                    }
+                    .padding()
+                    .foregroundColor(.gray)
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image(systemName: "paintbrush")
+                            .font(.system(size: 40))
+                    }
+                    .padding()
+                    .foregroundColor(.gray)
+                }
+            }
+                
+            
+        }
     }
 }
 
