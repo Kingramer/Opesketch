cfd0f314cd883d9ba49224451690ca8c7a8800dc
honma <honmatchy@icloud.com>
2021-10-19 12:20:25 +0900

    ダウンキャストによるエラーを改修

--- Opesketch/operationList.swift
+++ Opesketch/operationList.swift
@@ -15,9 +15,9 @@ struct operationList: View {
                 Array(repeating: .init(.fixed(20)), count: 2)*/
         ScrollView {
             LazyVGrid(columns: Array(repeating: GridItem(), count: 4)) { // カラム数の指定
-                ForEach((1...24), id: \.self) { index in
+                ForEach((0...23), id: \.self) { index in
                     Button(action: {}) {
-                        if index <= opeLen {
+                        if index < opeLen {
                             ZStack(alignment: .center) {
                                 RoundedRectangle(cornerRadius: 10)
                                     .stroke(.brown, lineWidth: 3)
@@ -28,7 +28,7 @@ struct operationList: View {
                         } else {
                             ZStack(alignment: .center) {
                                 RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.brown, style: StrokeStyle(lineWidth: 3, dash: [8]))
+                                    .stroke(.brown.opacity(0.5), style: StrokeStyle(lineWidth: 3, dash: [8]))
                                 opeList[index].image()
                             }
                             .frame(width: 65, height: 65)
@@ -53,63 +53,63 @@ enum OpeImageValue {
     case rotatel
     case big
     case small
-    func image() -> Image {
+    func image() -> some View {
         switch( self ){
             case .plus:
                 return Image(systemName: "plus")
                 .frame(width: 60.0, height: 60.0)
-                .foregroundColor(.brown)
-                .font(.system(size: 50)) as! Image
+                .foregroundColor(.brown.opacity(0.5))
+                .font(.system(size: 40)) /*as! Image*/
             case .right:
                 return Image(systemName: "arrow.right")
                 .frame(width: 60.0, height: 60.0)
                 .foregroundColor(.brown)
-                .font(.system(size: 50)) as! Image
+                .font(.system(size: 50))
             case .left:
                 return Image(systemName: "arrow.left")
                 .frame(width: 60.0, height: 60.0)
                 .foregroundColor(.brown)
-                .font(.system(size: 50)) as! Image
+                .font(.system(size: 50))
             case .up:
                 return Image(systemName: "arrow.up")
                 .frame(width: 60.0, height: 60.0)
                 .foregroundColor(.brown)
-                .font(.system(size: 50)) as! Image
+                .font(.system(size: 50))
             case .down:
                 return Image(systemName: "arrow.down")
                 .frame(width: 60.0, height: 60.0)
                 .foregroundColor(.brown)
-                .font(.system(size: 50)) as! Image
+                .font(.system(size: 50))
             case .turnr:
                 return Image(systemName: "arrow.turn.up.right")
                 .frame(width: 60.0, height: 60.0)
                 .foregroundColor(.brown)
-                .font(.system(size: 50)) as! Image
+                .font(.system(size: 50))
             case .turnl:
                 return Image(systemName: "arrow.turn.up.left")
                 .frame(width: 60.0, height: 60.0)
                 .foregroundColor(.brown)
-                .font(.system(size: 50)) as! Image
+                .font(.system(size: 50))
             case .rotater:
                 return Image(systemName: "arrow.clockwise")
                 .frame(width: 60.0, height: 60.0)
                 .foregroundColor(.brown)
-                .font(.system(size: 50)) as! Image
+                .font(.system(size: 50))
             case .rotatel:
                 return Image(systemName: "arrow.counterclockwise")
                 .frame(width: 60.0, height: 60.0)
                 .foregroundColor(.brown)
-                .font(.system(size: 50)) as! Image
+                .font(.system(size: 50))
             case .big:
                 return Image(systemName: "arrow.up.left.and.arrow.down.right")
                 .frame(width: 60.0, height: 60.0)
                 .foregroundColor(.brown)
-                .font(.system(size: 50)) as! Image
+                .font(.system(size: 50))
             case .small:
                 return Image(systemName: "arrow.down.right.and.arrow.up.left")
                 .frame(width: 60.0, height: 60.0)
                 .foregroundColor(.brown)
-                .font(.system(size: 50)) as! Image
+                .font(.system(size: 50))
         }
     }
 }
